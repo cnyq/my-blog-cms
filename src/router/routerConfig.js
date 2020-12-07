266d18813dba6115c6539fdcca002d8cb1035361
晏群 <cnyanqun@qq.com>
2020-12-07 17:17:50 +0800

    .

--- src/router/routerConfig.js
+++ src/router/routerConfig.js
@@ -1,14 +1,11 @@
 import router from '@/router'
 import NProgress from 'nprogress' // 页面加载进度条插件
 import 'nprogress/nprogress.css'
-import store from '../store'
+import store from '@/store'
 import {
-  setTitle
+  setCrumbList
 } from '@/utils/router-fun'
 
-import {
-  routerConstant
-} from "@/assets/mock/routerMock"
 NProgress.configure({
   showSpinner: false,
   minimum: 0.1,
@@ -18,46 +15,10 @@ NProgress.configure({
 const keepAliveArr = ['articleList']
 router.beforeEach(async (to, from, next) => {
   NProgress.start()
-  let currentRouter = routerConstant.filter(it => {
-      return it.router == to.path
-    }),
-    tit = currentRouter.length > 0 ? currentRouter[0].title : '',
-    updateCrumbList = []
-  setTitle(tit == '首页' ? '' : tit)
-  if (currentRouter.length > 0) {
-    let currentRouterObj = currentRouter[0]
-    if (currentRouterObj.grade == 1) {
-      if (keepAliveArr.includes(to.name)) {
-        store.dispatch('app/addKeepAlive', to.name)
-      }
-      if (currentRouterObj.router == '/home') {
-        updateCrumbList = [{
-          title: currentRouterObj.title,
-          router: currentRouterObj.router
-        }]
-      } else {
-        updateCrumbList = [{
-          title: '首页',
-          router: '/home'
-        }, {
-          title: currentRouterObj.title,
-          router: currentRouterObj.router
-        }]
-      }
-    } else if (currentRouterObj.grade == 2) {
-      updateCrumbList = [{
-        title: '首页',
-        router: '/home'
-      }, {
-        title: currentRouterObj.groupTitle,
-        router: ''
-      }, {
-        title: currentRouterObj.title,
-        router: currentRouterObj.router
-      }]
-    }
+  if (keepAliveArr.includes(to.name)) {
+    store.dispatch('app/addKeepAlive', to.name)
   }
-  store.dispatch("app/updateCrumbList", updateCrumbList)
+  setCrumbList(to)
   next()
 })
 
--- src/utils/router-fun.js
+++ src/utils/router-fun.js
@@ -1,9 +1,84 @@
-import defaultSetting from '../setting'
+import defaultSetting from '@/setting'
+import store from '@/store'
+import {
+  routerConstant,
+} from "@/assets/mock/routerMock"
 const { title } = defaultSetting
 
 export const pageTitle = title || 'cnyanqun'
 
 export const setTitle = function (meta) {
   document.title = meta || pageTitle
-  const ua = navigator.userAgent
+  // const ua = navigator.userAgent
+}
+
+const titStatus = ((type) => {
+  let tit = ''
+  switch (type) {
+    case 'add':
+      tit = '新增'
+      break
+  }
+  return tit
+})
+
+export const setCrumbList = (to) => {
+  console.log(to)
+  let currentRouter = routerConstant.filter(it => {
+    return it.router == to.path
+  }),
+    updateCrumbList = [],
+    tit = currentRouter.length > 0 ? currentRouter[0].title : ''
+  setTitle(tit == '首页' ? '' : tit)
+  if (currentRouter.length > 0) {
+    let currentRouterObj = currentRouter[0]
+    if (currentRouterObj.grade == 1) {
+      if (currentRouterObj.router == '/home') {
+        updateCrumbList = [{
+          title: currentRouterObj.title,
+          router: currentRouterObj.router
+        }]
+      } else {
+        updateCrumbList = [{
+          title: '首页',
+          router: '/home'
+        }, {
+          title: currentRouterObj.title,
+          router: currentRouterObj.router
+        }]
+      }
+    } else if (currentRouterObj.grade == 2) {
+      updateCrumbList = [{
+        title: '首页',
+        router: '/home'
+      }, {
+        title: currentRouterObj.groupTitle,
+        router: ''
+      }, {
+        title: currentRouterObj.title,
+        router: currentRouterObj.router
+      }]
+    }
+  } else {
+    let path = to.path.replace("Detali", ""),
+      arr = routerConstant.filter(it => {
+        return it.router == path
+      }),
+      sonTit = titStatus(to.query.type) || '详情'
+    setTitle(sonTit)
+    if (arr.length > 0) {
+      let obj = arr[0]
+      updateCrumbList = [{
+        title: '首页',
+        router: '/home'
+      }, {
+        title: obj.title,
+        router: obj.router
+      }, {
+        title: sonTit,
+        router: ''
+      }]
+    }
+  }
+  store.dispatch("app/updateCrumbList", updateCrumbList)
 }
