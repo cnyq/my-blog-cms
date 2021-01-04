7761f3715ab4cf8c5b2925c9f964a8eb3f036cf8
晏群 <cnyanqun@qq.com>
2021-01-04 20:51:48 +0800

    。

--- src/views/pagesManage/article/articlesDetali.vue
+++ src/views/pagesManage/article/articlesDetali.vue
@@ -40,6 +40,16 @@
           >
           </el-date-picker>
         </el-form-item>
+        <el-form-item label="关联tag：" prop="tag">
+          <template v-if="!form.tag || form.tag.length == 0">
+            <el-link type="primary" @click="openTagPop">选择关联tag</el-link>
+          </template>
+          <template v-else v-for="(item, index) in form.tag">
+            <el-tag size="medium" closable :key="index" @close="delChoiceTag">{{
+              item
+            }}</el-tag>
+          </template>
+        </el-form-item>
         <el-form-item label="上传文档：" prop="mdPath">
           <upLoadMd
             v-model="form.mdPath"
@@ -53,6 +63,58 @@
         </el-col>
       </el-row>
     </el-card>
+    <el-dialog
+      title="tag选择"
+      :visible.sync="tagListOpen"
+      @close="closeTagList"
+    >
+      <el-button type="primary" size="mini" @click="addTag" class="addTag"
+        >新建tag</el-button
+      >
+      <template v-if="isAddTag">
+        <el-form :inline="true" :model="tagParsms" label-position="left">
+          <el-form-item label="tag名称：">
+            <el-input
+              size="small"
+              v-model.trim="tagParsms.name"
+              placeholder="请输入文章名称"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" size="mini" @click="saveAddTag"
+              >保存</el-button
+            >
+            <el-button size="mini" @click="cancelAddTag">取消</el-button>
+          </el-form-item>
+        </el-form>
+      </template>
+      <template v-else>
+        <el-table
+          :data="tableData"
+          border
+          style
+          stripe
+          v-loading="tableLoading"
+          element-loading-text="努力加载中..."
+          element-loading-spinner="el-icon-loading"
+          element-loading-background="rgba(255, 255, 255, 0.5)"
+        >
+          <el-table-column
+            type="index"
+            label="序号"
+            width="80"
+          ></el-table-column>
+          <el-table-column prop="name" label="tag"></el-table-column>
+          <el-table-column label="操作" fixed="right" width="200">
+            <template slot-scope="scope">
+              <el-button size="small" type="text" @click="choiceTag(scope.name)"
+                >选择</el-button
+              >
+            </template>
+          </el-table-column>
+        </el-table>
+      </template>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -60,7 +122,14 @@ export default {
   name: "articleDetali",
   data() {
     return {
-      form: {},
+      form: {
+        name: "",
+        synopsis: "",
+        author: "",
+        writing_time: "",
+        tag: [],
+        mdPath: "",
+      },
       rules: {
         name: [{ required: true, message: "请输入文章名称", trigger: "blur" }],
         synopsis: [
@@ -72,9 +141,17 @@ export default {
         writing_time: [
           { required: true, message: "请输入选择写作时间", trigger: "change" },
         ],
-        mdPath: [{ required: true, message: "请上传文档", trigger: "change" }],
+        // mdPath: [{ required: true, message: "请上传文档", trigger: "change" }],
       },
       isEdit: false,
+      tagList: [],
+      tagListOpen: false,
+      isAddTag: false,
+      tagParsms: {
+        name: "",
+      },
+      tableLoading: false,
+      tableData: [],
     }
   },
   methods: {
@@ -87,7 +164,14 @@ export default {
           this.form.writing_time = this.form.writing_time - 0
           this.$axios.post("/acticleAdd", this.form).then((res) => {
             this.isEdit = true
-            this.$router.back(-1)
+            if (res.code == 200) {
+              this.$alert("保存成功", "提示", {
+                confirmButtonText: "确定",
+                callback: () => {
+                  this.$router.back(-1)
+                },
+              })
+            }
           })
         }
       })
@@ -95,6 +179,38 @@ export default {
     reuseValidate(val) {
       if (val) this.validate("mdPath")
     },
+    openTagPop() {
+      this.tagListOpen = true
+      this.getTagList()
+    },
+    getTagList() {
+      this.tableLoading = true
+      this.$axios.get("/tagList").then((res) => {
+        if (res.code == 200) {
+          this.tableData = res.data.list
+          this.tableLoading = false
+        }
+      })
+    },
+    delChoiceTag() {},
+    closeTagList() {
+      // console.log('aaa')
+    },
+    addTag() {
+      this.isAddTag = true
+    },
+    saveAddTag() {
+      this.$axios.post("/tagAdd", this.tagParsms).then((res) => {
+        if (res.code == 200) {
+          this.isAddTag = false
+          this.getTagList()
+        }
+      })
+    },
+    cancelAddTag() {
+      this.isAddTag = false
+    },
+    choiceTag() {},
   },
   beforeRouteLeave(to, from, next) {
     from.meta.isEdit = this.isEdit
@@ -102,3 +218,11 @@ export default {
   },
 }
 </script>
+<style lang="scss">
+.el-dialog__body {
+  padding: 0px 20px 30px;
+}
+.addTag {
+  margin-bottom: 20px;
+}
+</style>
