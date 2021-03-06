26bcc64093e631f369b29dca2fb74188b65a8edf
cnyanqun <cnyanqun@qq.com>
2021-03-06 23:43:36 +0800

    .

--- src/router/index.js
+++ src/router/index.js
@@ -28,7 +28,7 @@ const routes = [
   },{
     path: '/login',
     name: 'login',
-    component: () => import('@/components/pages/login1')
+    component: () => import('@/components/pages/login')
   },{
     path: '*', redirect: '/404', hidden: true
   },
